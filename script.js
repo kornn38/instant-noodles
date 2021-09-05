bdcad3cdfdb57b0e551ef142f520ec543d6fb257
kornn38 <kornn38@outlook.com>
2021-09-06 00:26:46 +0700

    Add the number going up feature, to work on localStorage

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log('TEST');
+let displayCups = document.querySelector('.display-qty');
+const addCups = document.querySelector('.add-qty');
+const removeCups = document.querySelector('.rem-qty');
+const clearCups = document.querySelector('.clear-qty');
+
+displayCups.value = localStorage.length;
+
+let currValue = displayCups.value;
+// console.log(currValue);
+
+const markup = `<h2>${currValue}</h2>`;
+
+addCups.addEventListener('click', function () {
+  currValue++;
+  displayCups.textContent = currValue;
+  //   console.log(currValue);
+  localStorage.setItem('number', currValue);
+  displayCups.insertAdjacentHTML('afterbegin', markup);
+  console.log(currValue);
+});
+removeCups.addEventListener('click', function () {
+  if (!currValue) return;
+  currValue--;
+  displayCups.textContent = currValue;
+  localStorage.setItem('number', currValue);
+});
+clearCups.addEventListener('click', function () {
+  currValue = 0;
+  displayCups.textContent = currValue;
+  console.log(currValue);
+  localStorage.clear('number');
+});
